b8072a3eb2589e5adc2a89785918c74f845c2917
VollodinGleb <glebvollodin@gmail.com>
2025-05-19 20:36:30 +0300

    test deploy

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage/HomePage';
+import OurCoffeePage from './components/OurCoffeePage/OurCoffeePage'
+import ForYourPleasurePage from './components/ForYourPleasurePage/forYourPleasurePage'
+import CoffeeItemPage from './components/CoffeItemPage/CoffeeItemPage'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <Routes>
+        <Route path="Coffee/" element={<HomePage />} />
+        <Route path="Coffee/our-coffee" element={<OurCoffeePage />} />
+        <Route path="Coffee/for-your-pleasure" element={<ForYourPleasurePage />} />
+         <Route path="Coffee/coffee-item/:id" element={<CoffeeItemPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
